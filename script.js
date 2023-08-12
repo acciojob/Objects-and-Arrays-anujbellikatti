a640e4c1b7fcccb78c95afece1f41f8de3903a83
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-08-12 21:30:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ const person = {
   age: 80,
 };
 
-const team = players;
-var team1;
-// Create a copy of the array players
-for(let i=0;i<players.length;i++){
-	team1 = team1 + players[i];
-}
-var cap1;
-for(let i=0;i<person.length;i++){
-	cap1 = cap1 + person[i];
-}
-console.log(team); // Output: ["John", "Bob", "Alice", "Poppy"]
-console.log(team1); // Output: ["John", "Bob", "Alice", "Poppy"]
-console.log(cap1); // Output: { name: "John Doe", age: 80 }
+// const team = players;
+// var team1;
+// // Create a copy of the array players
+// for(let i=0;i<players.length;i++){
+// 	team1 = team1 + players[i];
+// }
+// var cap1;
+// for(let i=0;i<person.length;i++){
+// 	cap1 = cap1 + person[i];
+// }
+var team= players;
+var team1 =players.slice();
+var cap1 = Object.assign({}, person);
 
 // Create a copy of the object person
 
